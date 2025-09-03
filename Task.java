04f6a7d9e02b4148098f8bbb86cecc1591c5d0d3
Breno Marques F. S. <brenomarques200@gmail.com>
2025-09-02 21:08:12 -0300

    Task.java

---
+++ Task.java
@@ -0,0 +1,20 @@
+package model;
+
+import java.io.Serializable;
+
+public class Task implements Serializable {
+    private String title;
+    private String description;
+    private boolean completed;
+
+    public Task(String title, String description) {
+        this.title = title;
+        this.description = description;
+        this.completed = false;
+    }
+
+    public String getTitle() { return title; }
+    public String getDescription() { return description; }
+    public boolean isCompleted() { return completed; }
+    public void setCompleted(boolean completed) { this.completed = completed; }
+}
